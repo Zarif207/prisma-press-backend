dbd4285413a851d70528f69227bd0acae128fc27
Zarif Hasan <zarifhasan207@gmail.com>
2026-06-23 18:17:14 +0600

    added configuration management and update server setup

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import app from "./app";
+import config from "./config";
+import "dotenv/config";
 
-const PORT = process.env.PORT || 4000;
+const PORT = config.port;
 
 async function main() {
   try {
+    // await prisma.$connect();
+    console.log("Connected to the database successfully.");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
+    // await prisma.$disconnect(); // Disconnect from the database in case of an error
     process.exit(1); // Exit the process with an error code
   }
 }
