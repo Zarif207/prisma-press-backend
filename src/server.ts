2ee4d373e23b18505192a6419f7cdc747518ef64
Zarif Hasan <zarifhasan207@gmail.com>
2026-06-23 20:27:09 +0600

    updated Prisma schema and migration files add user and profile models

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
 import app from "./app";
 import config from "./config";
 import "dotenv/config";
+import { prisma } from "./lib/prisma";
 
 const PORT = config.port;
 
 async function main() {
   try {
-    // await prisma.$connect();
+    await prisma.$connect();
     console.log("Connected to the database successfully.");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
-    // await prisma.$disconnect(); // Disconnect from the database in case of an error
+    await prisma.$disconnect(); // Disconnect from the database in case of an error
     process.exit(1); // Exit the process with an error code
   }
 }
