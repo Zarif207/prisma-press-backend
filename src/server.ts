5bb6071fdf96240cd1d2c598d4e7105c64521460
Zarif Hasan <zarifhasan207@gmail.com>
2026-06-23 14:33:55 +0600

    implement basic Express server with initial route and configuration

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from "./app";
+
+const PORT = process.env.PORT || 4000;
+
+async function main() {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process with an error code
+  }
+}
+
+main();
