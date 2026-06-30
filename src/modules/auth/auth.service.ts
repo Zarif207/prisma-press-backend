fc281839e24dc8d747d20c62e6e62730078440c9
Zarif Hasan <zarifhasan207@gmail.com>
2026-06-30 23:55:04 +0600

    added JWT token generation to loginUser function for authentication

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { ILoginUSer } from "./auth.interface";
 import { prisma } from "../../lib/prisma";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const loginUser = async (payload: ILoginUSer) => {
   const { email, password } = payload;
@@ -9,11 +10,24 @@ const loginUser = async (payload: ILoginUSer) => {
     where: { email },
   });
   const isPasswordMatched = await bcrypt.compare(password, user.password);
+
   if (!isPasswordMatched) {
     throw new Error("password is incorrect");
   }
 
-  return user;
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, "accessSecret", { expiresIn: "1d" });
+  const refreshToken = jwt.sign(jwtPayload, "refreshSecret", {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
 };
 
 export const authService = {
