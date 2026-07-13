47f116a9eaa3c97a137a61f472e14b959680095e
Zarif Hasan <zarifhasan207@gmail.com>
2026-07-13 19:45:49 +0600

    user registration logic and clean up unused code

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -10,7 +10,8 @@ router.post("/register", userController.registerUser )
 
 router.get("/me", auth(Role.ADMIN, Role.USER, Role.AUTHOR), userController.getMyProfile);
 
-
 router.put("/my-profile", auth(Role.ADMIN, Role.USER, Role.AUTHOR), userController.updateMyProfile);
+
+
 
 export const userRouter = router;
