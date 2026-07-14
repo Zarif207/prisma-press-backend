b720c6481bdcdfc80a243313a38ded764687e731
Zarif Hasan <zarifhasan207@gmail.com>
2026-07-14 19:36:50 +0600

    implement refresh token functionality in auth service and controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -33,6 +33,29 @@ const loginUser = catchAsync(
   },
 );
 
+const refreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    const { accessToken } = await authService.refreshToken(refreshToken);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 60 * 24, // 1 days
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Token refreshed successfully",
+      data: { accessToken },
+    });
+  },
+);
+
 export const authController = {
   loginUser,
+  refreshToken,
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import { SignOptions } from "jsonwebtoken";
+import { JwtPayload, SignOptions } from "jsonwebtoken";
 import config from "../../config";
 import { prisma } from "../../lib/prisma";
 import { jwtUtils } from "../../utils/jwt";
@@ -48,7 +48,7 @@ const loginUser = async (payload: ILoginUSer) => {
   const accessToken = jwtUtils.createToken(
     jwtPayload,
     config.jwt_access_secret,
-    config.jwt_access_expiration, 
+    config.jwt_access_expiration,
   );
 
   // const refreshToken = jwt.sign(
@@ -71,6 +71,45 @@ const loginUser = async (payload: ILoginUSer) => {
   };
 };
 
+const refreshToken = async (refreshToken: string) => {
+  const verifiedRefreshToken = jwtUtils.verifyToken(
+    refreshToken,
+    config.jwt_refresh_secret,
+  );
+
+  if (!verifiedRefreshToken.success) {
+    throw new Error(verifiedRefreshToken.error);
+  }
+
+  const { id } = verifiedRefreshToken.data as JwtPayload;
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
+
+  if (user.activeStatus === "BLOCKED") {
+    throw new Error("User is blocked!");
+  }
+
+  const jwtPayload = {
+    id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expiration,
+  );
+
+  return { accessToken };
+};
+
 export const authService = {
   loginUser,
+  refreshToken,
 };
